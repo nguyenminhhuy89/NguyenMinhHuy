5b398db39717ce5e8f171d439d39e961ae775200
nguyenminhhuy89 <nguyenminhhuy89@gmail.com>
2024-01-06 23:00:05 +0700

    update Sele

--- spec/selenium/relative_spec.rb
+++ spec/selenium/relative_spec.rb
@@ -3,12 +3,25 @@ require 'spec_helper'
 
 RSpec.describe 'Using Selenium for testing Relative selectors' do
   it 'uses eight components' do
-    driver = Selenium::WebDriver.for :firefox
-    driver.manage.timeouts.implicit_wait = 500
-    driver.get('https://www.selenium.dev/selenium/web/web-form.html')
+    begin
+      driver = Selenium::WebDriver.for :chrome
+      # wait = Selenium::WebDriver::Wait.new(timeout: 2) 
+      driver.manage.timeouts.implicit_wait = 500
+      driver.get('https://www.selenium.dev/selenium/web/web-form.html')
+      original_window = driver.window_handle
+      driver.switch_to.new_window(:tab)
+      expect(driver.window_handles.size).to eq 2
+      # Get reference element 
+      # txt = driver.find_element(xpath: '//input[@name="my-text"]')
+      # txt.clear
+      # txt.click
+      # txt.send_keys('12345')
 
-    password_locator = { relative: { name: 'my-datalist', left: { name: 'my-password' } } }
-    pass = driver.find_element(password_locator)
-    # pass.send_keys('New York')
+      # Find element above 
+      # above = driver.find_element(relative: {above: ref})
+      # above.send_keys('123456')
+    ensure
+      # driver.quit
+    end
   end
 end
