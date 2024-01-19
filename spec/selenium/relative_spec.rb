6b01b8d24cc9758972ccdf72909b32d9a4171898
nguyenminhhuy89 <nguyenminhhuy89@gmail.com>
2024-01-19 14:26:48 +0700

    add Selenium to CI

--- spec/selenium/relative_spec.rb
+++ spec/selenium/relative_spec.rb
@@ -3,6 +3,11 @@ require 'spec_helper'
 
 RSpec.describe 'Using Selenium for testing Relative selectors' do
   it 'uses eight components' do
+    options = Selenium::WebDriver::Options.send "chrome"
+    driver = Selenium::WebDriver.for(:remote,
+      :url => "https://#{USER_NAME}:#{ACCESS_KEY}@hub.browserstack.com/wd/hub",
+      :capabilities => options
+    )
     begin
       driver = Selenium::WebDriver.for :chrome
       # wait = Selenium::WebDriver::Wait.new(timeout: 2) 
@@ -11,17 +16,40 @@ RSpec.describe 'Using Selenium for testing Relative selectors' do
       original_window = driver.window_handle
       driver.switch_to.new_window(:tab)
       expect(driver.window_handles.size).to eq 2
-      # Get reference element 
-      # txt = driver.find_element(xpath: '//input[@name="my-text"]')
-      # txt.clear
-      # txt.click
-      # txt.send_keys('12345')
-
-      # Find element above 
-      # above = driver.find_element(relative: {above: ref})
-      # above.send_keys('123456')
     ensure
-      # driver.quit
+      driver.quit
     end
   end
+
+  BUILD_NAME = "browserstack-build-1"
+  caps = [{
+  "os" => "Windows",
+  "osVersion" => "11",
+  "browserName" => "Chrome",
+  "browserVersion" => "103.0",
+  "buildName" => BUILD_NAME,
+  "sessionName" => "Ruby thread 1"
+  },
+  {
+  "os" => "Windows",
+  "osVersion" => "10",
+  "browserName" => "Firefox",
+  "browserVersion" => "102.0",
+  "buildName" => BUILD_NAME,
+  "sessionName" => "Ruby thread 2"
+  },
+  {
+  "os" => "OS X",
+  "osVersion" => "Big Sur",
+  "browserName" => "Safari",
+  "browserVersion" => "14.1",
+  "buildName" => BUILD_NAME,
+  "sessionName" => "Ruby thread 3"
+  }]
+  t1 = Thread.new{ run_session(caps[0]) }
+  t2 = Thread.new{ run_session(caps[1]) }
+  t3 = Thread.new{ run_session(caps[2]) }
+  t1.join()
+  t2.join()
+  t3.join()
 end
